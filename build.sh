90f14eccd1cfc0fae726babc5977d95b9282e7c3
Cyb3r-Jak3 <git@Cyberjake.xyz>
2023-02-06 18:39:04 -0500

    One last attempt at not hiding files

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
-python --version
-pip install pipenv
-mv Pipfile.hidden Pipfile
-mv Pipfile.lock.hidden Pipfile.lock
-pipenv sync
+# python --version
+# pip install pipenv
+# mv Pipfile.hidden Pipfile
+# mv Pipfile.lock.hidden Pipfile.lock
+# pipenv sync
 make html
 cp /opt/build/bin/run-build-functions.sh build/html
